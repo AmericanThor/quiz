4fec05380c8393b41c12da7a89421e1a0b555b69
AmericanThor <105684121+AmericanThor@users.noreply.github.com>
2023-03-25 09:35:57 -0500

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+    numb: 1,
+    question: "How far away is the moon?",
+    answer: "238,855 miles",
+    options: [
+      "238,855 miles",
+      "1,800 miles",
+      "~1,000,000 miles",
+      "735,035 miles"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What state has the longest freshwater shoreline?",
+    answer: "Michigan",
+    options: [
+      "Florida",
+      "Michigan",
+      "New York",
+      "Wisconsin"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Which blood type is a universal donor?",
+    answer: "O-",
+    options: [
+      "A-",
+      "B+",
+      "O-",
+      "O+"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which singer’s real name is Robyn Fenty?",
+    answer: "Rihanna",
+    options: [
+      "Bono",
+      "Cher",
+      "Prince",
+      "Rihanna"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What Minnesota town was actor Winona Ryder born in?",
+    answer: "Winona",
+    options: [
+      "Duluth",
+      "Minneapolis",
+      "St. Paul",
+      "Winona"
+    ]
+  },
+];
